4890bdc37d65b7497da9c434f83124446137dfc9
jacob lourie <jakelourie1502@gmail.com>
2022-01-07 16:26:43 +0000

    removes print policy frm utils

--- utils.py
+++ utils.py
@@ -1,25 +1,4 @@
 import numpy as np
-def print_policy(env, policy_array):
-    """Takes an environment and an array of size (env.n_states, ) 
-    ReShapes array to env.h, env.w
-    Changes [0,1,2,3] to ["^", ">", "<", "v"]
-    Prints the policy
-    """
-    action_dict = {0:"^", 1: ">", 2: "v", 3: "<"}
-    policy_board = np.zeros((env.h , env.w), dtype = str)
-    for s in range(env.n_states-1):
-        coors = env.stateIdx_to_coors[s]
-        policy_board[coors] = action_dict[policy_array[s]]
-    print(policy_board)
-    
-def print_state_vals(env, values):
-    policy_board = np.zeros((env.h , env.w))
-    for s in range(env.n_states-1):
-        coors = env.stateIdx_to_coors[s]
-        policy_board[coors] = values[s]
-    print(policy_board)
-
-
 
 def check_optimal(optimal_policy,policy,env):
     if len(policy)<20:
